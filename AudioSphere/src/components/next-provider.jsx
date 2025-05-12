d5b792c7b1787c023c149e2ac23eebd939ef5c88
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-13 03:55:24 +0530

    IndoLike

--- AudioSphere/src/components/next-provider.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useState } from "react";
-import { NextContext } from "@/hooks/use-context";
-export default function NextProvider({ children }) {
-  const [nextData, setNextData] = useState(null);
-  return <NextContext.Provider value={{ nextData, setNextData }}>{children}</NextContext.Provider>;
-}
