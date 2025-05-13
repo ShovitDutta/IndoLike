300f4f9f16b3ad1a500e7afb74841f0bc785c204
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-13 12:14:51 +0530

    IndoLike
    IndoLike

---
+++ AudioSphere/ignore/src/hooks/use-context.jsx
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+"use client";
+import { createContext } from "react";
+export const MusicContext = createContext(null);
+export const NextContext = createContext(null);
+=======
+"use client";
+import { createContext } from "react";
+export const MusicContext = createContext(null);
+export const NextContext = createContext(null);
+>>>>>>> 8c930d8c52fe0e2eeb08f275e5a181aea5f59fce
